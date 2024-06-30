bddc67105bc18838096618394ad26f6692c313d3
Enzorcand <enzorcand@gmail.com>
2024-06-30 13:25:25 -0300

    Logica inicial

---
+++ src/main/java/org/example/GraphReader.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class GraphReader {
+    private Scanner scanner;
+    private void readArchive() {
+
+        try {
+            File file = new File("src/main/java/org/example/testes/teste10.txt");
+        }
+        catch (Exception e){
+
+        }
+        return;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,18 @@
 package org.example;
 
+import org.example.graphs.Digraph;
+import org.example.graphs.In;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println();
+        Digraph graph = new Digraph(10);
+        HashMap<Integer, String> aurelio = new HashMap<>();
+        Scanner scan;
     }
+
+
 }
